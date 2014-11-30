930d959368b69b21dde486a2d03d7256199a1add
Ravi <Ravi@FEAR>
2014-11-29 22:08:02 -0500

    Semi - final code

--- ComputerNetworks/src/computer/networks/project/main/GroupNameQ2Client.java
+++ ComputerNetworks/src/computer/networks/project/main/GroupNameQ2Client.java
@@ -2,10 +2,13 @@ package computer.networks.project.main;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.ObjectOutput;
+import java.io.ObjectOutputStream;
 import java.io.PrintWriter;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
@@ -15,22 +18,31 @@ import java.net.InetSocketAddress;
 import java.net.Socket;
 import java.net.SocketException;
 import java.net.SocketTimeoutException;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Scanner;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 /*
  * Client class
  */
-public class GroupNameQ2Client implements Runnable {
+public class GroupNameQ2Client {
 
 	// Instance variables
 	private Scanner userInput;
-	private String givenUserInpt;
+	private String[] givenUserInpt = new String[5];
 	private boolean isAckReceived; // boolean value that determines if ack has
 	// been
 	// received
 	private boolean transferDone = false; // boolean flag to indicate when it is
-	// done
+	protected static final String FILE_KEY = "FILE";
+	protected static final String BUFFER_KEY = "BUFFER";
+	protected static final String TIMEOUT_KEY = "TIMEOUT";
+	protected static final String SYSTEM_PORT_KEY = "PORT";
 	private byte[] dataBuffer; // Represents buffer that holds data to be sent
 	private int bufferSize; // Represents buffer size
 	private int fileSize; // Represents file size
@@ -38,42 +50,42 @@ public class GroupNameQ2Client implements Runnable {
 	private int NBE = 0; // Next expected byte
 	private int S; // Start sequence
 	private int E; // End sequence
-	private final int systemPortToBeUsed = 55555; // Port used for communication
+	private Integer systemPortToBeUsed = 12345; // Port used for communication
 	private DatagramSocket socketEntry; // Reference for socket used to send and
 	// receive packets
 	private DatagramPacket packetsSent; // Reference for packets sent
 	private DatagramPacket packetsReceived; // Reference for packets received
 	private InetAddress hostIPAddress; // Reference to the Receiver's IP details
-	private long t1, t2;
+	private static ArrayList<String> listOfWebAddress;
+	private static int currentWebAddressToProcess = 0;
+	public static final String LAST_PACKET = "LAST_PACKET";
+	private final Lock lock = new ReentrantLock();
 
 	// Method main
 	public static void main(String[] args) {
-		// TODO code application logic here
 		GroupNameQ2Client client = new GroupNameQ2Client();
 		client.processUserInput();
 
 		// checks the input
-		if (client.givenUserInpt.equalsIgnoreCase("local")) {
-			client.getUDPInput();
+		if ("localhost".equalsIgnoreCase(client.givenUserInpt[3])) {
+			client.setBufferAndTimeoutValues();
 			client.processLocalHost();
-		} else {
-			client.initThreads();
 		}
 	}
 
 	// Method getServerInput: used to read input from user
 	public void processUserInput() {
 		userInput = new Scanner(System.in);
-		int choice = (int) getUserInput(Boolean.TRUE);
+		int choice = Integer.parseInt((String) getUserInput(Boolean.TRUE));
 		if (choice == 1) {
 			this.initThreads();
-		}else{
-			get
+		} else {
+			getUserInput(Boolean.FALSE);
 		}
 	}
 
 	public enum InputTypes {
-		FILE, BUFFER_SIZE, TIMEOUT, FILE_ERROR, IS_OPTION_CORRECT, ENTER_WELCOME_OPTION
+		FILE, BUFFER_SIZE, TIMEOUT, FILE_ERROR, IS_OPTION_CORRECT, ENTER_WELCOME_OPTION, PORT_NUMBER
 	}
 
 	private Object getUserInput(boolean isWelcomeMessageTobeDisplayed) {
@@ -82,8 +94,14 @@ public class GroupNameQ2Client implements Runnable {
 			return validateUserInput(userInput.nextInt(), InputTypes.IS_OPTION_CORRECT);
 		} else {
 			System.out.println("Please enter the file path of the file you want to send");
-			String filePath = userInput.next();
-
+			String filePath = (String) validateUserInput(userInput.next(), InputTypes.FILE);
+			givenUserInpt[0] = filePath;
+			String bufferSize = validateUserInput(null, InputTypes.BUFFER_SIZE).toString();
+			givenUserInpt[1] = bufferSize;
+			givenUserInpt[3] = new String("localhost");
+			givenUserInpt[2] = validateUserInput(null, InputTypes.TIMEOUT).toString();
+			givenUserInpt[4] = validateUserInput(null, InputTypes.PORT_NUMBER).toString();
+			return null;
 		}
 	}
 
@@ -96,7 +114,7 @@ public class GroupNameQ2Client implements Runnable {
 				File userFile = new File((String) givenInput);
 				if (userFile.isFile()) {
 					isUserValueCorrect = Boolean.TRUE;
-					continue;
+					return userFile.getAbsolutePath();
 				} else {
 					currentOptionToCheck = InputTypes.FILE_ERROR;
 					continue;
@@ -109,88 +127,119 @@ public class GroupNameQ2Client implements Runnable {
 			case BUFFER_SIZE:
 				System.out.println("Please enter the Buffer Size");
 				int input = userInput.nextInt();
+				return (new Integer(input));
 			case IS_OPTION_CORRECT:
-				int userChoiceInput = (int) givenInput;
+				Integer userChoiceInput = (int) givenInput;
 				if (userChoiceInput == 1 || userChoiceInput == 2) {
 					isUserValueCorrect = Boolean.TRUE;
-					return userInput;
+					return userChoiceInput.toString();
 				} else {
 					currentOptionToCheck = InputTypes.ENTER_WELCOME_OPTION;
 					continue;
 				}
 			case ENTER_WELCOME_OPTION:
 				System.out.print("Please enter a choice, \n 1. Web Address. \n 2.Localhost. \n Please enter either 1 or 2");
-				givenInput = userInput.nextInt();
-				currentOptionToCheck = InputTypes.IS_OPTION_CORRECT;
+				if (userInput.hasNextInt()) {
+					givenInput = userInput.nextInt();
+					currentOptionToCheck = InputTypes.IS_OPTION_CORRECT;
+				} else {
+					System.out.println("Please enter only Number 1 or 2");
+					currentOptionToCheck = InputTypes.ENTER_WELCOME_OPTION;
+				}
 				continue;
 			case TIMEOUT:
 				System.out.println("Please enter an integer value to set timeout in Milliseconds");
 				this.timeout = userInput.nextInt();
 				isUserValueCorrect = Boolean.TRUE;
+				return this.timeout;
+			case PORT_NUMBER:
+				System.out.println("Please enter the port number you want to send as a Number, example 12345");
+				if (userInput.hasNextInt()) {
+					return userInput.nextInt();
+				} else {
+					System.out.println("Please enter only Number like 12345 or 21234");
+					currentOptionToCheck = InputTypes.PORT_NUMBER;
+				}
 				continue;
 			}
 		}
 		return null;
 	}
 
-	// Method getUDPInput: used to read input from user
-	public void getUDPInput() {
-
-		givenUserInpt = userInput.nextLine();
-		bufferSize = Integer.parseInt(givenUserInpt.split(" ")[1]); // set
-		// bufferSize
-		timeout = Integer.parseInt(givenUserInpt.split(" ")[2]); // set timeout
-		// value
+	public void setBufferAndTimeoutValues() {
+		bufferSize = Integer.parseInt(givenUserInpt[1]);
+		timeout = Integer.parseInt(givenUserInpt[2]);
 	}
 
 	// The thread handler method
 	public void initThreads() {
-		Thread th = new Thread(this, "Thread1");
-		Thread th2 = new Thread(this, "Thread2");
-		th.start();
-		th2.start();
-		try {
-			th.join();
-			th2.join();
-		} catch (InterruptedException ex) {
+
+		listOfWebAddress = new ArrayList<String>();
+		getUserInputForWebAddress(InputTypes.ENTER_WELCOME_OPTION);
+		int numberOfThreadsToCreate = listOfWebAddress.size();
+		ExecutorService totalThreadsToCreate = Executors.newFixedThreadPool(numberOfThreadsToCreate);
+
+		for (int i = 0; i < numberOfThreadsToCreate; i++) {
+			totalThreadsToCreate.submit(new Runnable() {
+				@Override
+				public void run() {
+					runLogic();
+				}
+			});
 		}
-		System.out.println("Request time (Milliseconds)");
-		System.out.println("1\t" + t1);
-		System.out.println("2\t" + t2);
+		totalThreadsToCreate.shutdown();
 	}
 
-	@Override
-	public void run() {
-		long startTime, endTime;
+	public void getUserInputForWebAddress(InputTypes key) {
+		switch (key) {
+		case ENTER_WELCOME_OPTION:
+			System.out.println("please enter the web addresses you want to send the GET message");
+			if (userInput.hasNext()) {
+				listOfWebAddress.add(userInput.next());
+			}
+			System.out.println("Do you want to enter another address ? Please answer in true or false only!");
+			if (userInput.hasNextBoolean()) {
+				boolean userRespnse = userInput.nextBoolean();
+				if (userRespnse) {
+					key = InputTypes.ENTER_WELCOME_OPTION;
+					getUserInputForWebAddress(key);
+				} else {
+					break;
+				}
+			}
+			break;
+		default:
+			break;
+		}
+	}
+
+	public void runLogic() {
 
 		String name = Thread.currentThread().getName();
+		int currentIndex = getCurrentWebAddressRequestToProcessId();
 		String response;
-		startTime = System.currentTimeMillis();
-		File output;
 		Socket socket = new Socket();
 		BufferedWriter bWriter = null;
 		BufferedReader bReader = null;
-
-		int number = name.endsWith("Thread1") ? 1 : 2;
-		output = new File("WebResponse_" + number + ".txt");
+		File file = new File("GET_RESPONSE_OF_" + listOfWebAddress.get(currentIndex) + ".txt");
+		System.out.println("Printing output for thread =[" + name + "For the url =[" + listOfWebAddress.get(currentIndex) + "]");
 
 		try {
-			socket.connect(new InetSocketAddress(givenUserInpt, 80));
+			socket.connect(new InetSocketAddress(listOfWebAddress.get(currentIndex), 80));
 			bWriter = new BufferedWriter(new PrintWriter(socket.getOutputStream(), true));
 			bReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 			bWriter.write("GET / HTTP/1.0\r\n\r\n");
 			bWriter.flush();
 			System.out.println(name + ": Request Sent");
-			bWriter = new BufferedWriter(new FileWriter(output));
-			bWriter.write(givenUserInpt + "\r\n");
+			bWriter = new BufferedWriter(new FileWriter(file));
+			bWriter.write(listOfWebAddress.get(currentIndex) + "\r\n");
 			do {
 				response = bReader.readLine();
 				if (response != null) {
+					System.out.println(response);
 					bWriter.write(response + "\r\n");
 				}
 			} while (response != null);
-			bWriter.flush();
-
 		} catch (Exception ex) {
 			System.out.println(name + ": Error occured: " + ex.getMessage());
 			System.exit(-1);
@@ -203,12 +252,16 @@ public class GroupNameQ2Client implements Runnable {
 
 			}
 		}
+	}
 
-		endTime = System.currentTimeMillis();
-		if (name.equals("Thread1")) {
-			t1 = endTime - startTime;
-		} else {
-			t2 = endTime - startTime;
+	public int getCurrentWebAddressRequestToProcessId() {
+		try {
+			lock.lock();
+			int index = currentWebAddressToProcess;
+			currentWebAddressToProcess++;
+			return index;
+		} finally {
+			lock.unlock();
 		}
 	}
 
@@ -236,7 +289,12 @@ public class GroupNameQ2Client implements Runnable {
 		// keeps sending the input entered by user in one packet until ack is
 		// received
 		do {
-			dataBuffer = givenUserInpt.getBytes(); // loading buffer to be sent
+
+			dataBuffer = getBufferForDataToSend();// loading
+			// buffer
+			// to
+			// be
+			// sent
 			sendPacket();
 			receivePacket();
 			if (!isAckReceived) {
@@ -272,13 +330,39 @@ public class GroupNameQ2Client implements Runnable {
 				System.out.println("\nReceived sequence number " + S + " to " + E);
 				System.out.println(new String(Arrays.copyOfRange(dataBuffer, 2, dataBuffer.length)).trim());
 				System.out.println();
+			} else if (isAckReceived) {
+				String dataReceieved = new String(Arrays.copyOfRange(dataBuffer, 2, dataBuffer.length)).trim();
+				if (LAST_PACKET.equals(dataReceieved)) {
+					transferDone = true;
+				}
 			}
 			dataBuffer = new byte[2];
 			dataBuffer[0] = (byte) (NBE / 127);
 			dataBuffer[1] = (byte) (NBE % 127);
 			sendPacket();
+			if (dataBuffer[0] < 0) {
+				transferDone = true;
+			}
 		} while (!transferDone);
 		System.out.println("DONE!!!");
+		System.exit(-1);
+	}
+
+	protected byte[] getBufferForDataToSend() {
+		HashMap<String, String> dataToSend = new HashMap<String, String>();
+		dataToSend.put(FILE_KEY, givenUserInpt[0]);
+		dataToSend.put(BUFFER_KEY, givenUserInpt[1]);
+		dataToSend.put(TIMEOUT_KEY, givenUserInpt[2]);
+		dataToSend.put(SYSTEM_PORT_KEY, givenUserInpt[4]);
+		ByteArrayOutputStream byteOutputStream = new ByteArrayOutputStream();
+		ObjectOutput objectOutput = null;
+		try {
+			objectOutput = new ObjectOutputStream(byteOutputStream);
+			objectOutput.writeObject(dataToSend);
+		} catch (IOException e) {
+			System.out.println("Something went wrong here =[" + e.getMessage() + "]");
+		}
+		return byteOutputStream.toByteArray();
 	}
 
 	// Method to send packets
@@ -286,9 +370,13 @@ public class GroupNameQ2Client implements Runnable {
 		packetsSent = new DatagramPacket(dataBuffer, dataBuffer.length, hostIPAddress, systemPortToBeUsed);
 		try {
 			socketEntry.send(packetsSent);
+			systemPortToBeUsed = Integer.parseInt(givenUserInpt[4]);
 		} catch (IOException ex) {
-			System.out.println("Error sending packet");
+			System.out.println("Data Packet Not Sent, the error message is =[" + ex.getMessage() + "]");
+		} catch (NullPointerException ex) {
+			System.out.println("Something went Wrong and error is =[" + ex.getMessage() + "]");
 		}
+
 	}
 
 	// Method to receive packets
@@ -307,4 +395,5 @@ public class GroupNameQ2Client implements Runnable {
 			System.out.println("Error receiving packet");
 		}
 	}
+
 }
--- ComputerNetworks/src/computer/networks/project/main/GroupNameQ2Server.java
+++ ComputerNetworks/src/computer/networks/project/main/GroupNameQ2Server.java
@@ -1,13 +1,15 @@
 package computer.networks.project.main;
 
+import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
-import java.net.InetAddress;
 import java.net.SocketException;
+import java.util.HashMap;
 
 /*
  * Server class
@@ -17,18 +19,17 @@ public class GroupNameQ2Server {
 	private DatagramSocket socket;
 	private DatagramPacket recvPacket;
 	private DatagramPacket sendPacket;
-	private InetAddress host;
 	private String ackMsg;
-	private int timeout;
-	private int B;
+	private int timeoutValueSetByClient;
+	private int byteSizeBeingSentByClient;
 	private int ackNo = 0;
 	private int NBE = 0;
 	private int E = 0;
 	private boolean ackRecvd; // boolean value that determines if ack has been
-								// received
+	// received
 	private boolean flag = true;
-	private final int PORT = 55555;
-	private File file;
+	private static int PORT = 12345;
+	private File fileTOFetchByServer;
 	private FileReader fReader;
 	private byte[] buffer; // Represents buffer that holds data
 	private int bufferSize; // Represents buffer size
@@ -43,38 +44,38 @@ public class GroupNameQ2Server {
 	public void processRequest() {
 		try {
 			socket = new DatagramSocket(PORT);
-			host = InetAddress.getByName("localhost");
 		} catch (Exception ex) {
+			System.out.println(ex.getMessage());
 		}
 		buffer = new byte[1400]; // Initialize buffer array
 		// receive first packet : User input from client
 		System.out.println("Waiting for Packets");
 		receivePacket(); // receive packets
 		System.out.println("Packet received \n");
-		// Set file, B and timeout based on user input received from packet
-		file = new File(new String(recvPacket.getData()).split(" ")[0]);
-		B = Integer.parseInt(new String(recvPacket.getData()).split(" ")[1]);
-		timeout = Integer.parseInt(new String(recvPacket.getData()).trim().split(" ")[2]);
 
+		intializeParamters();
 		// Printing their values
 		System.out.println("_______________________________");
-		System.out.println("File name: " + file.getName());
-		System.out.println("B: " + B);
-		System.out.println("T: " + timeout);
+		System.out.println("Name of File requested by Client: " + fileTOFetchByServer.getName());
+		System.out.println("Number of Bytes of Data Sent: " + byteSizeBeingSentByClient);
+		System.out.println("Timeout Value set by Client: " + timeoutValueSetByClient);
+		System.out.println("Timeout Value set by Client: " + recvPacket.getAddress().getHostAddress());
+
 		System.out.println("_______________________________");
 
 		// Prepare acknowledgment
-		ackMsg = "Ack " + file.length();
+		ackMsg = "Ack " + fileTOFetchByServer.length();
 		buffer = ackMsg.getBytes();
 		// Send ack
 		sendPacket();
 
 		try {
-			fReader = new FileReader(file);
+			fReader = new FileReader(fileTOFetchByServer);
 		} catch (FileNotFoundException ex) {
 		}
 		try {
-			socket.setSoTimeout(2 * timeout); // Set server timeout
+			socket.setSoTimeout(2 * timeoutValueSetByClient); // Set server
+			// timeout
 		} catch (SocketException ex) {
 		}
 
@@ -84,10 +85,10 @@ public class GroupNameQ2Server {
 			// check if client has read data before reloading buffer
 			if (flag) {
 				loadBuffer();
-				if ((NBE + B) >= file.length()) {
-					E = (int) file.length();
+				if ((NBE + byteSizeBeingSentByClient) >= fileTOFetchByServer.length()) {
+					E = (int) fileTOFetchByServer.length();
 				} else {
-					E = NBE + B - 1;
+					E = NBE + byteSizeBeingSentByClient - 1;
 				}
 			}
 			do {
@@ -100,16 +101,44 @@ public class GroupNameQ2Server {
 			// ack less than 0 indicates done
 			if (ackNo < 0) {
 				System.out.println("DONE!!!");
+				sendDonePacket();
 				break;
 			}
 			if (E + 1 == ackNo) {
-				NBE += B;
+				NBE += byteSizeBeingSentByClient;
 				flag = true;
 			} else {
 				flag = false;
 			}
 
 		} while (true);
+
+	}
+
+	@SuppressWarnings("unchecked")
+	public void intializeParamters() {
+		HashMap<String, String> dataReceieved = new HashMap<String, String>();
+		// Set file, B and timeout based on user input received from packet
+		ByteArrayInputStream byteIntputStream = new ByteArrayInputStream(buffer);
+		ObjectInputStream objectIntput = null;
+		try {
+			objectIntput = new ObjectInputStream(byteIntputStream);
+			dataReceieved = (HashMap<String, String>) objectIntput.readObject();
+			PORT = Integer.parseInt(dataReceieved.get(GroupNameQ2Client.SYSTEM_PORT_KEY));
+			fileTOFetchByServer = new File(dataReceieved.get(GroupNameQ2Client.FILE_KEY));
+			byteSizeBeingSentByClient = Integer.parseInt(dataReceieved.get(GroupNameQ2Client.BUFFER_KEY));
+			timeoutValueSetByClient = Integer.parseInt(dataReceieved.get(GroupNameQ2Client.TIMEOUT_KEY));
+			socket.close();
+			socket = new DatagramSocket(PORT);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (NullPointerException ex) {
+			System.out.println("Oops something went wrong , the message is =[" + ex.getMessage() + "]");
+		}
 
 	}
 
@@ -135,12 +164,24 @@ public class GroupNameQ2Server {
 		}
 	}
 
+	public void sendDonePacket() {
+		buffer = GroupNameQ2Client.LAST_PACKET.getBytes();
+		bufferSize = buffer.length;
+		bufferSize = bufferSize + 2;
+		try {
+			sendPacket = new DatagramPacket(buffer, bufferSize, recvPacket.getAddress(), recvPacket.getPort());
+			socket.send(sendPacket);
+		} catch (Exception ex) {
+			// System.out.println("Error sending packet");
+		}
+	}
+
 	// Method used to load buffer from file
 	public void loadBuffer() {
-		if (file.length() > (B + NBE)) {
-			buffer = new byte[B + 2];
+		if (fileTOFetchByServer.length() > (byteSizeBeingSentByClient + NBE)) {
+			buffer = new byte[byteSizeBeingSentByClient + 2];
 		} else {
-			buffer = new byte[(int) (file.length() - NBE) + 2];
+			buffer = new byte[(int) (fileTOFetchByServer.length() - NBE) + 2];
 		}
 
 		int i = 0, pos = 2;
